0e030011bdd7c064d9ca7dfd0b6f1d434ba843fb
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-06 13:24:34 +0300

    Add MapController, where Foo, InnerBar, InnerFizz, and SubInnerBuzz mappings are performed on demand

---
+++ src/NetCore.AutoMapperPrototype.App.Test.Unit/Controllers/MapControllerTest.cs
@@ -0,0 +1,177 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NetCore.AutoMapperPrototype.App.Controllers;
+using NetCore.AutoMapperPrototype.App.Data.Dtos;
+using NetCore.AutoMapperPrototype.App.Data.Entities;
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+namespace NetCore.AutoMapperPrototype.App.Test.Unit.Controllers
+{
+    public sealed class MapControllerTest
+    {
+        private readonly MapController mapController;
+        private readonly Mock<IMapper> mockMapper;
+
+        public MapControllerTest()
+        {
+            mockMapper = new Mock<IMapper>();
+            mapController = new MapController(mockMapper.Object);
+        }
+
+        [Fact]
+        internal void GivenTestFooMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<Foo>(It.IsAny<FooDto>()))
+                .Returns(It.IsAny<Foo>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestFooMapping(It.IsAny<FooDto>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<Foo>());
+        }
+
+        [Fact]
+        internal void GivenTestFoosMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<IEnumerable<Foo>>(It.IsAny<ICollection<FooDto>>()))
+                .Returns(It.IsAny<IEnumerable<Foo>>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestFoosMapping(It.IsAny<ICollection<FooDto>>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<IEnumerable<Foo>>());
+        }
+
+        [Fact]
+        internal void GivenTestBarMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<InnerBar>(It.IsAny<InnerBarDto>()))
+                .Returns(It.IsAny<InnerBar>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestBarMapping(It.IsAny<InnerBarDto>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<InnerBar>());
+        }
+
+        [Fact]
+        internal void GivenTestBarsMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<IEnumerable<InnerBar>>(It.IsAny<ICollection<InnerBarDto>>()))
+                .Returns(It.IsAny<IEnumerable<InnerBar>>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestBarsMapping(It.IsAny<ICollection<InnerBarDto>>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<IEnumerable<InnerBar>>());
+        }
+
+        [Fact]
+        internal void GivenTestFizzMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<InnerFizz>(It.IsAny<InnerFizzDto>()))
+                .Returns(It.IsAny<InnerFizz>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestFizzMapping(It.IsAny<InnerFizzDto>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<InnerFizz>());
+        }
+
+        [Fact]
+        internal void GivenTestFizzesMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<IEnumerable<InnerFizz>>(It.IsAny<ICollection<InnerFizzDto>>()))
+                .Returns(It.IsAny<IEnumerable<InnerFizz>>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestFizzesMapping(It.IsAny<ICollection<InnerFizzDto>>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<IEnumerable<InnerFizz>>());
+        }
+
+        [Fact]
+        internal void GivenTestBuzzMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<SubInnerBuzz>(It.IsAny<SubInnerBuzzDto>()))
+                .Returns(It.IsAny<SubInnerBuzz>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestBuzzMapping(It.IsAny<SubInnerBuzzDto>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<SubInnerBuzz>());
+        }
+
+        [Fact]
+        internal void GivenTestBuzzesMappingCalledWhenInputIsValidThenReturnsMappedData()
+        {
+            // Arrange
+            mockMapper
+                .Setup(_ => _.Map<IEnumerable<SubInnerBuzz>>(It.IsAny<ICollection<SubInnerBuzzDto>>()))
+                .Returns(It.IsAny<IEnumerable<SubInnerBuzz>>())
+                .Verifiable();
+
+            // Act
+            var result = mapController.TestBuzzesMapping(It.IsAny<ICollection<SubInnerBuzzDto>>());
+
+            // Assert
+            mockMapper.Verify();
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be((int)HttpStatusCode.OK);
+            (result as OkObjectResult).Value.Should().Be(It.IsAny<IEnumerable<SubInnerBuzz>>());
+        }
+    }
+}
--- src/NetCore.AutoMapperPrototype.App/Controllers/MapController.cs
+++ src/NetCore.AutoMapperPrototype.App/Controllers/MapController.cs
@@ -1,6 +1,89 @@
-﻿namespace NetCore.AutoMapperPrototype.App.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.AutoMapperPrototype.App.Data.Dtos;
+using NetCore.AutoMapperPrototype.App.Data.Entities;
+using System.Collections.Generic;
+
+namespace NetCore.AutoMapperPrototype.App.Controllers
 {
-    public sealed class MapController
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class MapController : ControllerBase
     {
+        private readonly IMapper mapper;
+
+        public MapController(IMapper mapper) => this.mapper = mapper;
+
+        [HttpPost]
+        [Route("api/v1/map/test-foo")]
+        public IActionResult TestFooMapping([FromBody] FooDto dto)
+        {
+            var foo = mapper.Map<Foo>(dto);
+
+            return Ok(foo);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-foos")]
+        public IActionResult TestFoosMapping([FromBody] ICollection<FooDto> dtos)
+        {
+            var foos = mapper.Map<IEnumerable<Foo>>(dtos);
+
+            return Ok(foos);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-bar")]
+        public IActionResult TestBarMapping([FromBody] InnerBarDto dto)
+        {
+            var bar = mapper.Map<InnerBar>(dto);
+
+            return Ok(bar);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-bars")]
+        public IActionResult TestBarsMapping([FromBody] ICollection<InnerBarDto> dtos)
+        {
+            var bars = mapper.Map<IEnumerable<InnerBar>>(dtos);
+
+            return Ok(bars);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-fizz")]
+        public IActionResult TestFizzMapping([FromBody] InnerFizzDto dto)
+        {
+            var fizz = mapper.Map<InnerFizz>(dto);
+
+            return Ok(fizz);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-fizzes")]
+        public IActionResult TestFizzesMapping([FromBody] ICollection<InnerFizzDto> dtos)
+        {
+            var fizzes = mapper.Map<IEnumerable<InnerFizz>>(dtos);
+
+            return Ok(fizzes);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-buzz")]
+        public IActionResult TestBuzzMapping([FromBody] SubInnerBuzzDto dto)
+        {
+            var buzz = mapper.Map<SubInnerBuzz>(dto);
+
+            return Ok(buzz);
+        }
+
+        [HttpPost]
+        [Route("api/v1/map/test-buzzes")]
+        public IActionResult TestBuzzesMapping([FromBody] ICollection<SubInnerBuzzDto> dtos)
+        {
+            var buzzes = mapper.Map<IEnumerable<SubInnerBuzz>>(dtos);
+
+            return Ok(buzzes);
+        }
     }
 }
