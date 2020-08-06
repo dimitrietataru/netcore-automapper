81930ca596042e94d9bc1ce07addabc7278cd6cd
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-06 12:40:00 +0300

    Configure AutoMapper Profile

--- README.md
+++ README.md
@@ -8,7 +8,40 @@ PM> Install-Package AutoMapper.Extensions.Microsoft.DependencyInjection -Version
 
 ### Configure Profiles
 ``` csharp
-// ..
+public class AutoMapperProfile : AutoMapper.Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<TSource, TDestionation>();
+        CreateMap<TDestionation, TSource>();
+        
+        CreateMap<Foo, Bar>();
+        CreateMap<Bar, Foo>();
+        
+        CreateMap<Fizz, Buzz>().ReverseMap();
+        
+        CreateMap<FooDto, Foo>()
+            .IgnoreAllPropertiesWithAnInaccessibleSetter()
+            .ForMember(
+                destination => destination.StringValue,
+                options => options.MapFrom(source => source.StringValue))
+            .ForMember(
+                destination => destination.IntValue,
+                options => options.MapFrom(source => source.IntValue))
+            .ForMember(
+                destination => destination.DoubleValue,
+                options => options.MapFrom(source => source.DoubleValue))
+            .ForMember(
+                destination => destination.BooleanValue,
+                options => options.MapFrom(source => source.BooleanValue))
+            .AfterMap(
+                (source, destination) =>
+                {
+                    // ..
+                })
+            .ForAllOtherMembers(options => options.Ignore());
+    }
+}
 ```
 
 ### Configure Dependency Injection
---
+++ src/NetCore.AutoMapperPrototype.App/Data/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using NetCore.AutoMapperPrototype.App.Data.Dtos;
+using NetCore.AutoMapperPrototype.App.Data.Entities;
+using System.Diagnostics;
+
+namespace NetCore.AutoMapperPrototype.App.Data.Mapping
+{
+    [DebuggerStepThrough]
+    public sealed class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<FooDto, Foo>()
+                .IgnoreAllPropertiesWithAnInaccessibleSetter()
+                .ForMember(
+                    destination => destination.StringValue,
+                    options => options.MapFrom(source => source.StringValue))
+                .ForMember(
+                    destination => destination.IntValue,
+                    options => options.MapFrom(source => source.IntValue))
+                .ForMember(
+                    destination => destination.DoubleValue,
+                    options => options.MapFrom(source => source.DoubleValue))
+                .ForMember(
+                    destination => destination.BooleanValue,
+                    options => options.MapFrom(source => source.BooleanValue))
+                .ForMember(
+                    destination => destination.Bar,
+                    options => options.MapFrom(source => source.Bar))
+                .ForMember(
+                    destination => destination.Fizzes,
+                    options => options.MapFrom(source => source.Fizzes))
+                .ForAllOtherMembers(options => options.Ignore());
+
+            CreateMap<InnerBarDto, InnerBar>()
+                .IgnoreAllPropertiesWithAnInaccessibleSetter()
+                .ForMember(
+                    destination => destination.Value,
+                    options => options.MapFrom(source => source.Value))
+                .ForAllOtherMembers(options => options.Ignore());
+
+            CreateMap<InnerFizzDto, InnerFizz>()
+                .IgnoreAllPropertiesWithAnInaccessibleSetter()
+                .ForMember(
+                    destination => destination.Value,
+                    options => options.MapFrom(source => source.Value))
+                .ForMember(
+                    destination => destination.Buzzes,
+                    options => options.MapFrom(source => source.Buzzes))
+                .ForAllOtherMembers(options => options.Ignore());
+
+            CreateMap<SubInnerBuzzDto, SubInnerBuzz>()
+                .IgnoreAllPropertiesWithAnInaccessibleSetter()
+                .ForMember(
+                    destination => destination.Value,
+                    options => options.MapFrom(source => source.Value))
+                .ForAllOtherMembers(options => options.Ignore());
+        }
+    }
+}
